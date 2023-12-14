48d7399394dc382254e2a0e12e895ab8697edc38
PyronSu <tspaing699@gmail.com>
2023-12-14 07:45:07 +0630

    home page ui

---
+++ resources/views/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    {{-- flowbite JS cdn link --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
+    {{-- font awesome link --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @vite('resources/css/app.css')
+</head>
+<body class="h-screen overflow-hidden">
+    @yield('content')
+    <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+</body>
+</html>

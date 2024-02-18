fb551228f8cb3dcf5417fbed6c7a8d2b634e952f
ThaeSuPaing02 <tspaing699@gmail.com>
2024-02-19 01:57:55 +0630

    installed laravel breeze to this project

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\JournalController;
-use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,28 +8,13 @@ use App\Http\Controllers\CategoryController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/',[CategoryController::class,'home'])->name('journal#home');
-Route::post('journal/home',[CategoryController::class,'home'])->name('journal#homepage');
-Route::post('/get/journal',[JournalController::class,'getJournals']);
-
-Route::post('journal/category',[CategoryController::class,'createCategory'])->name('create#Category');
-
-Route::get('/journal/create',[JournalController::class,'create'])->name('create#journal');
-Route::post('/journal/save',[JournalController::class,'save'])->name('save#journal');
-Route::get('/journal/read',[JournalController::class,'read'])->name('read#journal');
-Route::get('/settings',[CategoryController::class,'setting'])->name('setting#setting');
-
-//cateogry store
-Route::post('store',[CategoryController::class,'store']);
-Route::post('/edit-category',[CategoryController::class,'edit'])->name('edit-category');
-Route::post('delete',[CategoryController::class,'delete'])->name('category#delete');
-
-//layout from Dev Team
-Route::get('/layout',function(){
-    return view('layout');
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
 });
+
+require __DIR__.'/auth.php';
